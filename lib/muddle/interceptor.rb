4f071b0865f7f06bcf872c87f70e6193088db19a
Ben Hamill <git-commits@benhamill.com>
2012-03-22 02:05:50 -0500

    Place-holder Railtie stuff so we can hook into ActionMailer.

---
+++ lib/muddle/interceptor.rb
@@ -0,0 +1,5 @@
+module Muddle::Interceptor
+  def self.delivering_email(message)
+    # Do something with the Mail::Message instance here.
+  end
+end
---
+++ lib/muddle/rails.rb
@@ -0,0 +1,10 @@
+require File.expand_path(File.dirname(__FILE__) + '/../muddle')
+require File.expand_path(File.dirname(__FILE__) + 'interceptor')
+
+module Texticle
+  class Railtie < Rails::Railtie
+    initializer "muddle.configure_rails_initialization" do
+      ActionMailer::Base.register_interceptor(Muddle::Interceptor)
+    end
+  end
+end
