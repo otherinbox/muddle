ef5f61559cac638d8271bb27c29de7d2cb015fe7
Ben Hamill <git-commits@benhamill.com>
2012-03-22 02:15:39 -0500

    Can you tell where I cribbed this from?

--- lib/muddle/rails.rb
+++ lib/muddle/rails.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/../muddle')
 require File.expand_path(File.dirname(__FILE__) + 'interceptor')
 
-module Texticle
+module Muddle
   class Railtie < Rails::Railtie
     initializer "muddle.configure_rails_initialization" do
       ActionMailer::Base.register_interceptor(Muddle::Interceptor)
