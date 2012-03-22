3127f505e5d33513fad2bc355076e301e5596b5d
Ryan Michael <kerinin@gmail.com>
2012-03-22 08:56:42 -0500

    adding rails app in examples folder

---
+++ examples/rails_example/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsExample::Application.config.secret_token = 'f89d82ed423020b56dd92c098b6e85db7a865f606a675df62e68f0c508178cad70a9bc98aae3e015fde8ec1d216d9c36a676d3cb07718f4e81957fdb55615e40'
