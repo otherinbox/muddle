86d9b8f1500d0bb40a4dc30103a1a6b796e53184
Ryan Michael <kerinin@gmail.com>
2012-03-26 08:49:16 -0500

    cleaning up

--- lib/muddle/configuration.rb
+++ lib/muddle/configuration.rb
@@ -1,47 +1,45 @@
-module Muddle
-  class Configuration
-    attr_accessor :parse_with_premailer
-    attr_accessor :insert_boilerplate_styles
-    attr_accessor :insert_boilerplate_css
-    attr_accessor :insert_boilerplate_attributes
-    attr_accessor :validate_html
-    attr_accessor :generate_plain_text
-    
-    attr_accessor :premailer
+class Muddle::Configuration
+  attr_accessor :parse_with_premailer
+  attr_accessor :insert_boilerplate_styles
+  attr_accessor :insert_boilerplate_css
+  attr_accessor :insert_boilerplate_attributes
+  attr_accessor :validate_html
+  attr_accessor :generate_plain_text
+  
+  attr_accessor :premailer
 
-    # Initialize the configuration object with default values
-    #
-    # if a block is passed, we'll yield 'this' to it so you can set config values
-    #     
-    def initialize(&block)
-      @parse_with_premailer = true
-      @insert_boilerplate_styles = true
-      @insert_boilerplate_css = true
-      @insert_boilerplate_attributes = true
-      @validate_html = true
-      @generate_plain_text = false
+  # Initialize the configuration object with default values
+  #
+  # if a block is passed, we'll yield 'this' to it so you can set config values
+  #     
+  def initialize(&block)
+    @parse_with_premailer = true
+    @insert_boilerplate_styles = true
+    @insert_boilerplate_css = true
+    @insert_boilerplate_attributes = true
+    @validate_html = true
+    @generate_plain_text = false
 
-      # NOTE: when this tries to inline CSS, all it sees is a stylesheet URL
-      # This may require that we download css from the interwebs @ each render 
-      # pass of a mailer ?!?!?
-      @premailer = {
-        #:base_url => root_url, # Put in rails stuff...
-        :remove_comments => true, # Env-dependent?
-        :with_html_string => true,
-        :adapter => :nokogiri
-      }
+    # NOTE: when this tries to inline CSS, all it sees is a stylesheet URL
+    # This may require that we download css from the interwebs @ each render 
+    # pass of a mailer ?!?!?
+    @premailer = {
+      #:base_url => root_url, # Put in rails stuff...
+      :remove_comments => true, # Env-dependent?
+      :with_html_string => true,
+      :adapter => :nokogiri
+    }
 
-      if block_given?
-        configure &block
-      end
+    if block_given?
+      configure &block
     end
+  end
 
-    # Set config vars
-    #
-    # Pass it a block, will yield the config object
-    #
-    def configure
-      yield self
-    end
+  # Set config vars
+  #
+  # Pass it a block, will yield the config object
+  #
+  def configure
+    yield self
   end
 end
--- lib/muddle/parser.rb
+++ lib/muddle/parser.rb
@@ -1,30 +1,28 @@
-module Muddle
-  class Parser
-    attr_accessor :filters
+class Muddle::Parser
+  attr_accessor :filters
 
-    # Initialize the parser
-    #
-    # base is the parent muddle instance, and must be passed so the parser
-    #     can access the parse settings
-    #
-    def initialize(config=Muddle.config)
-      @filters = []
+  # Initialize the parser
+  #
+  # base is the parent muddle instance, and must be passed so the parser
+  #     can access the parse settings
+  #
+  def initialize(config=Muddle.config)
+    @filters = []
 
-      @filters << Muddle::BoilerplateCSSFilter if config.insert_boilerplate_css
-      @filters << Muddle::PremailerFilter if config.parse_with_premailer
-      @filters << Muddle::BoilerplateStyleElementFilter if config.insert_boilerplate_styles
-      @filters << Muddle::BoilerplateAttributesFilter if config.insert_boilerplate_attributes
-      @filters << Muddle::SchemaValidationFilter if config.validate_html
-    end
+    @filters << Muddle::BoilerplateCSSFilter if config.insert_boilerplate_css
+    @filters << Muddle::PremailerFilter if config.parse_with_premailer
+    @filters << Muddle::BoilerplateStyleElementFilter if config.insert_boilerplate_styles
+    @filters << Muddle::BoilerplateAttributesFilter if config.insert_boilerplate_attributes
+    @filters << Muddle::SchemaValidationFilter if config.validate_html
+  end
 
-    # Parse an email body
-    #
-    # body_string is the email body to be parsed in string form
-    #
-    # Returns the parsed body string
-    #
-    def parse(body_string)
-      filters.inject(body_string) {|filtered_string, filter| filter.filter(filtered_string)}
-    end
+  # Parse an email body
+  #
+  # body_string is the email body to be parsed in string form
+  #
+  # Returns the parsed body string
+  #
+  def parse(body_string)
+    filters.inject(body_string) {|filtered_string, filter| filter.filter(filtered_string)}
   end
 end
