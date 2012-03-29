623b7028c07aaeb945fc282dae227b2431e7f8cd
Ryan Michael <kerinin@gmail.com>
2012-03-29 10:00:33 -0500

    adding comment explaining filters in parser#init

--- lib/muddle/parser.rb
+++ lib/muddle/parser.rb
@@ -1,6 +1,28 @@
 class Muddle::Parser
   attr_accessor :filters
 
+  # Set up the parser
+  #
+  # The default filters do the following:
+  #
+  # CSS Filter adds a style block containing boilerplate CSS attributes to be
+  #   inlined.  This is based on Email Boilerplate's 'Inline: YES' portions
+  #
+  # Premailer passes the email through the Premailer gem, which inlines the CSS
+  #   it can, then appends a style block to the body containing the rest (since
+  #   some email clients strip out the <head> tag).
+  #
+  # Style Element adds another style block, but this one is intended to be left
+  #   as a style declaration (rather than being inlined).  This is based on 
+  #   EMail Boilerplate's 'Inline: NO' portions
+  #
+  # Attributes adds attributes to HTML elements where helpful, such as table cellpadding
+  #   and such.  Based on Email Boilerplate's example element declarations
+  #
+  # Schema Validation currently just adds the XHTML Strict DTD and outputs to a string,
+  #   however it's intended that this will eventually validate against an XSD and emit
+  #   warnings about potentially troublesome tags (like <div>)
+  #
   def initialize
     @filters = []
 
