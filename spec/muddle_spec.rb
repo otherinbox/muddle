b5e140d6d5f6683423f0b7d128d28a5fc5e54cba
Ben Hamill <git-commits@benhamill.com>
2012-04-02 21:18:33 -0500

    Break up the default options tests.

--- spec/muddle_spec.rb
+++ spec/muddle_spec.rb
@@ -5,18 +5,36 @@ describe Muddle do
     Muddle.parser.should be_a(Muddle::Parser)
   end
 
-  it "has a config with default options" do
-    Muddle.config.parse_with_premailer.should be_true
-    Muddle.config.insert_boilerplate_css.should be_true
-    Muddle.config.validate_html.should be_true
-    Muddle.config.generate_plain_text.should be_false
-    Muddle.config.premailer.should eql({
-      :remove_comments => true,
-      :with_html_string => true,
-      :adapter => :hpricot
-    })
+  context "with default options" do
+    it "uses premailer" do
+      Muddle.config.parse_with_premailer.should be_true
+    end
+
+    it "inserts boiletplate css" do
+      Muddle.config.insert_boilerplate_css.should be_true
+    end
+
+    it "validates the html" do
+      Muddle.config.validate_html.should be_true
+    end
+
+    it "doesn't generate plaintext" do
+      Muddle.config.generate_plain_text.should be_false
+    end
+
+    it "configures premailer to remove comments" do
+      Muddle.config.premailer[:remove_comments].should be_true
+    end
+
+    it "configures premailer to expect input as a string" do
+      Muddle.config.premailer[:with_html_string].should be_true
+    end
+
+    it "configures premailer to use hpricot" do
+      Muddle.config.premailer[:adapter].should == :hpricot
+    end
   end
-  
+
   it "constructs the root module with a configure method" do
     Muddle.configure do |config|
       config.parse_with_premailer = false
