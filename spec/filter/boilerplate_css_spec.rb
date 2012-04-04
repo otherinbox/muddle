b2580d4ac174e1f81f0c838e5c9216756fc9b380
Ben Hamill <git-commits@benhamill.com>
2012-04-04 00:40:02 -0500

    Reorg the boilerplate css specs.

--- spec/filter/boilerplate_css_spec.rb
+++ spec/filter/boilerplate_css_spec.rb
@@ -1,45 +1,81 @@
 require 'spec_helper'
 
 describe Muddle::Filter::BoilerplateCSS do
-  let(:f) { Muddle::Filter::BoilerplateCSS }
+  context "with a minimal email" do
+    subject { Muddle::Filter::BoilerplateCSS.filter(minimal_email_body) }
 
-  it "can parse full documents" do
-    output = f.filter(minimal_email_body)
+    it "has only one head" do
+      xpath(subject, '//head').count.should eql(1)
+    end
 
-    xpath(output, '//head').count.should eql(1)
-    xpath(output, '//style').count.should eql(2)
-    xpath(output, 'html/head/style[@type="text/css"]').first.content.should include('Boilerplate CSS for Inlining')
-    output.should have_xpath('/html/body/preceding-sibling::head')
+    it "inserts an extra style tag" do
+      xpath(subject, '//style').count.should eql(2)
+    end
+
+    it "inserts the boilerplate css into the head" do
+      xpath(subject, 'html/head/style[@type="text/css"]').first.content.should include('Boilerplate CSS for Inlining')
+    end
+
+    it "keeps the head before the body" do
+      subject.should have_xpath('/html/body/preceding-sibling::head')
+    end
   end
 
-  it "adds <head> and a <style> tag if needed" do
-    email = "<html><body><h1>foo</h1><body></html>"
+  context "with no head tag" do
+    let(:email) { "<html><body><h1>foo</h1><body></html>" }
 
-    output = f.filter(email)
+    subject { Muddle::Filter::BoilerplateCSS.filter(email) }
 
-    output.should have_xpath('html')
-    output.should have_xpath('html/head')
-    output.should have_xpath('html/head/style[@type="text/css"]')
-    xpath(output, 'html/head/style[@type="text/css"]').first.content.should include('Boilerplate CSS for Inlining')
-    output.should have_xpath('/html/body/preceding-sibling::head')
+    it "retains the html tag" do
+      subject.should have_xpath('html')
+    end
+
+    it "adds a head tag inside the html tag" do
+      subject.should have_xpath('html/head')
+    end
+
+    it "adds a style tag into the head tag" do
+      subject.should have_xpath('html/head/style[@type="text/css"]')
+    end
+
+    it "puts the boilerplate css into the style tag" do
+      xpath(subject, 'html/head/style[@type="text/css"]').first.content.should include('Boilerplate CSS for Inlining')
+    end
+
+    it "puts the head tag before the body" do
+      subject.should have_xpath('/html/body/preceding-sibling::head')
+    end
   end
 
-  it "adds the <style> tag before existing one" do
-    email = "<html><head><style></style></head> <h1>foo</h1></html>"
+  context "with an existing style in a head tag" do
+    let(:email) { "<html><head><style></style></head> <h1>foo</h1></html>" }
 
-    output = f.filter(email)
+    subject { Muddle::Filter::BoilerplateCSS.filter(email) }
 
-    xpath(output, 'html/head/style').count.should eql(2)
-    xpath(output, 'html/head/style[@type="text/css"]').should be_true
-    xpath(output, 'html/head/style[@type="text/css"]').first.content.should include("Boilerplate CSS for Inlining")
+    it "adds a second style tag" do
+      xpath(subject, 'html/head/style').count.should eql(2)
+    end
+
+    it "inserts a style with type text/css" do
+      xpath(subject, 'html/head/style').first.attribute('type').value.should == 'text/css'
+    end
+
+    it "inserts the boilerplate css into the first style tag" do
+      xpath(subject, 'html/head/style').first.content.should include("Boilerplate CSS for Inlining")
+    end
   end
 
-  it "adds the <style> tag if none are defined" do
-    email = "<html><head></head> <h1>foo</h1></html>"
+  context "with a head and no style tag" do
+    let(:email) { "<html><head></head> <h1>foo</h1></html>" }
 
-    output = f.filter(email)
+    subject { Muddle::Filter::BoilerplateCSS.filter(email) }
 
-    xpath(output, 'html/head/style[@type="text/css"]').should be_true
-    xpath(output, 'html/head/style[@type="text/css"]').first.content.should include('Boilerplate CSS for Inlining')
+    it "inserts a style tag" do
+      xpath(subject, 'html/head/style[@type="text/css"]').should be_true
+    end
+
+    it "inserts the boilerplate css into the style tag" do
+      xpath(subject, 'html/head/style[@type="text/css"]').first.content.should include('Boilerplate CSS for Inlining')
+    end
   end
 end
