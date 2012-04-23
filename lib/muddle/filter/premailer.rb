1d8584fae4db1165f20a413f04bfb3dbc8b96864
Ben Hamill <git-commits@benhamill.com>
2012-04-23 14:33:58 -0500

    Not puts, warn.

--- lib/muddle/filter/premailer.rb
+++ lib/muddle/filter/premailer.rb
@@ -4,7 +4,8 @@ module Muddle::Filter::Premailer
   def self.filter(body_string)
     premailer = Premailer.new(body_string, Muddle.config.premailer_options)
 
-    puts "Premailer generated #{premailer.warnings.length.to_s} warnings:" unless premailer.warnings.empty?
+    warn "Premailer generated #{premailer.warnings.length.to_s} warnings:" unless premailer.warnings.empty?
+
     premailer.warnings.each do |w|
       puts "#{w[:message]} (#{w[:level]}) may not render properly in #{w[:clients]}"
     end
